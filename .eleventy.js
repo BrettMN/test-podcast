f9854decafc5e9cc63bf6362149531ad4ae78581
Brett Nelson <brett@WIPDeveloper.com>
2021-03-14 22:07:03 -0500

    test with new feeds update

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,6 @@
 // Import plugin
 const feedsPlugin = require('eleventy-plugin-feeds');
+const { stripHtml } = require('eleventy-plugin-wipdeveloper-tools/src/');
 
 module.exports = function (eleventyConfig) {
   /*
@@ -14,29 +15,74 @@ module.exports = function (eleventyConfig) {
     // directory that will hold the templates for the feeds.
     // dirForFeeds: 'feeds-plugin-test',
     // Full site url without a trailing slash.
-    siteUrl: `https://test.com`,
+    siteUrl: `https://test-podcast.netlify.app`,
     // Site title
-    siteTitle: 'Test Site',
+    siteTitle: 'Test Podcast Site',
     // Site description
     siteDescription: 'Fake site description for test rss ',
     // Language
     language: 'en',
     // Full Url to site image
-    siteImage: `https://test.com/full/path/to/image.png`,
+    siteImage: `https://test-podcast.netlify.app/full/path/to/image.png`,
     // Full Favicon path
-    favicon: 'https://test.com/favicon.ico',
+    favicon: 'https://test-podcast.netlify.app/favicon.ico',
     // Copyright info
-    copyright: `All rights reserved ${new Date().getFullYear()}, Tester McTesterson`,
+    copyright: `All rights reserved ${new Date().getFullYear()}, Just Some Apps LLC`,
     // Categories
-    categories: ['test', 'this'],
+    categories: ['test', 'Podcast'],
     // Author Name
-    authorName: 'Tester McTesterson',
+    authorName: 'Brett Nelson',
     // Author Email
-    authorEmail: 'Tester@McTesterson.co',
+    authorEmail: 'brett@wipdeveloper.com',
     // Author URl
-    authorUrl: `https://test.com/me`,
+    authorUrl: `https://test-podcast.netlify.app/`,
     // Featured image meta tag from each post
-    imagePropertyName: 'featuredImage',
+    // imagePropertyName: 'featuredImage',
+
+    feedOptions: {
+      // Full site url without a trailing slash.
+      siteUrl: `https://test-podcast.netlify.app`,
+      // Site title
+      siteTitle: 'Test Podcast Site',
+      // Site description
+      siteDescription: 'Fake site description for test rss ',
+      // Language
+      language: 'en',
+      // Full Url to site image
+      siteImage: `https://test-podcast.netlify.app/full/path/to/image.png`,
+      // Full Favicon path
+      favicon: 'https://test-podcast.netlify.app/favicon.ico',
+      // Copyright info
+      copyright: `All rights reserved ${new Date().getFullYear()}, Just Some Apps LLC`,
+      // Categories
+      categories: ['test', 'Podcast'],
+      // Author Name
+      authorName: 'Brett Nelson',
+      // Author Email
+      authorEmail: 'brett@wipdeveloper.com',
+      // Author URl
+      authorUrl: `https://test-podcast.netlify.app/`,
+      // Featured image meta tag from each post
+      //   imagePropertyName: 'featuredImage',
+    },
+    feedItemMapper: function (itemFromCollection) {
+      return {
+        title: itemFromCollection.data.title,
+        description: stripHtml(itemFromCollection.templateContent).substring(
+          0,
+          200
+        ),
+        url: `https://test-podcast.netlify.app${itemFromCollection.url}`, // link to the item
+        categories: itemFromCollection.data.tags, // optional - array of item categories
+        date: itemFromCollection.data.date, // any format that js Date can parse.
+        content: itemFromCollection.templateContent, // optional string Long html content for the episode
+
+        enclosure: {
+          url: `https://test-podcast.netlify.app${itemFromCollection.data.audio}`,
+          //   file: `https://test-podcast.netlify.app${itemFromCollection.data.audio}`,
+        }, // optional enclosure
+      };
+    },
   });
 
   return {
--- src/episode/one.md
+++ src/episode/one.md
@@ -1,6 +1,8 @@
 ---
 title: first episode
 tags: episode
+template: base.11ty.js
+audio: /audio/uploads/Kalimba.mp3
 ---
 
 something something
